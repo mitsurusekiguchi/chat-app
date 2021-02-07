06fb90c81f3d38011e68b43601fdd67cddee1001
chuntarou <chuntarou@chun.com>
2021-02-07 18:12:20 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
   belongs_to :comment, dependent: :destroy
 
   validates :content, presence: true
